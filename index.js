4b1a41598d23e339f34a079b774f1f5cf38f2810
Austin Bailey <baileyau@bc.edu>
2018-02-21 22:17:11 -0500

    Files load in from init.json

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const {app, BrowserWindow} = require('electron')
-const url = require('url')
-const path = require('path')
-const shell = require('electron').shell
+const {app, BrowserWindow, ipcMain} = require('electron');
+const url = require('url');
+const path = require('path');
+const shell = require('electron').shell;
 
 let win
 
 function createWindow() {
    win = new BrowserWindow({width: 800, height: 600, titleBarStyle: 'hidden', frame: false});
    win.setMenu(null);
+   win.webContents.send('init-data', 'hello!');
    win.loadURL(url.format ({
       pathname: path.join(__dirname, 'index.html'),
       protocol: 'file:',
       slashes: true
-   }))
+  }));
 }
 
 app.on('ready', createWindow)
