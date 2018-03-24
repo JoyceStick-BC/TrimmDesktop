488d8a2481f079268e400028a201d19daa0bc343
Austin Bailey <baileyau@bc.edu>
2018-03-24 15:37:02 -0400

    post processing

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function createWindow() {
 
    win.webContents.send('init-data', 'hello!');
    win.loadURL(url.format ({
-      pathname: path.join(__dirname, 'redirect.html'),
+      pathname: path.join(__dirname, 'index.html'),
       protocol: 'file:',
       slashes: true
   }));
